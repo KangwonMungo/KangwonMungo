c8ca24fa5a573bd4ee4c9ca80bfb5ab399eaaef3
KKPASII <kkongpasi1005@gmail.com>
2025-05-20 23:38:39 +0900

    refactor: 코드 정리 및 테스트 메인 함수 추가
    안 쓰는 함수 제거
    
    안 쓰는 라이브러리 제거
    
    타입 힌트 추가
    
    주석 삭제

--- rag/chunking.py
+++ rag/chunking.py
@@ -1,35 +1,18 @@
 import pandas as pd
 import re
-import kss
-
-CHUNK_SIZE = 2
-INPUT_CSV_PATH = "./aladin_bestseller.csv"
-
-all_chunk_data = []
-
-# CSV 파일 열기
-try:
-    book_csv = pd.read_csv("./aladin_bestseller.csv", dtype=str) ### 파일 경로 확인
-except FileNotFoundError:
-    print("Error: aladin_bestseller.csv 파일을 찾을 수 없습니다. 경로를 확인해주세요.")
-    exit()
-except Exception as e:
-    print(f"CSV 파일을 읽는 중 오류 발생: {e}")
-    exit()
+from typing import List, Dict, Set, Any
 
 
-# "[]" 형식 문자열 -> list
-def parse_list_from_string(s):
-    if s == "" or s.strip() == "[]": # 빈 문자열, "[]"
+def parse_list_from_string(s: str) -> List[str]:
+    if s == "" or s.strip() == "[]":
         return []
     
-    str_list = re.findall(r"'(.*?)'", s) # ''(작은 따옴표) 안에 있는 모든 문자열이 담긴 리스트 반환
+    str_list = re.findall(r"'(.*?)'", s)
     return str_list
 
 
-# "{}" 형식 문자열 -> set
-def parse_set_from_string(s):
-    if s == "" or s.strip() == "{}": # 빈 문자열, "{}"
+def parse_set_from_string(s: str) -> Set[str]:
+    if s == "" or s.strip() == "{}":
         return set()
     
     str_list = re.findall(r"'(.*?)'", s)
@@ -37,29 +20,10 @@ def parse_set_from_string(s):
     return str_set
 
 
-# chunking 함수 - 문장 'CHUNK_SIZE'개씩 chunking
-def chunk_sentences(sentence_list, chunk_size):
-    all_chunks = []
-    
-    if not sentence_list: # 빈 리스트
-        return all_chunks
-
-    for i in range(0, len(sentence_list), chunk_size):
-        selected_sentences_list = sentence_list[i:i + chunk_size] # 리스트 슬라이싱: 인덱스 범위를 넘어가도 오류 없이 가능한 끝까지 가져옴
-        sentence_chunk = " ".join(selected_sentences_list)
-        all_chunks.append(sentence_chunk)
-    
-    # 청크가 만들어지지 않은 경우 (e.g. 길이가 1인데 chunk_sizer가 2)
-    if not all_chunks:
-        all_chunks.append(" ".join(sentence_list)) # 하나의 청크로 만들기
-    return all_chunks
-
-
-def chunk_file_by_line():
-    global all_chunk_data
+def chunk_file_by_line(book_df: pd.DataFrame) ->  List[Dict[str, Any]]:
     all_chunk_data = []
     
-    for index, row in book_csv.iterrows():
+    for index, row in book_df.iterrows():
         try:
             title = str(row.get('title', ''))
             author = str(row.get('author', ''))
@@ -92,22 +56,39 @@ def chunk_file_by_line():
         }
         all_chunk_data.append(chunk_data)
 
+    return all_chunk_data
 
-def print_all_chunk_data():
-    global all_chunk_data
-    # 만들어진 chunk 확인
+
+def print_all_chunk_data(all_chunk_data: List[Dict[str, Any]]) -> None:
     if all_chunk_data:
-        for i, data_item in enumerate(all_chunk_data):
+        for i, chunk in enumerate(all_chunk_data):
             print(f"\nProcessed Chunk {i+1}:")
-            print(f"  Title: {data_item.get('title', '')}")
-            print(f"  Author: {data_item.get('author', '')}")
-            print(f"  Keywords: {data_item.get('keywords', [])}")
-            print(f"  Genres: {data_item.get('genres', [])}")
-            print(f"  Chunk Index: {data_item.get('chunk_index', '')}")
-            print(f"  Introduction Chunk: {data_item.get('introduction', '')}")
+            print(f"  Title: {chunk.get('title', '')}")
+            print(f"  Author: {chunk.get('author', '')}")
+            print(f"  Keywords: {chunk.get('keywords', [])}")
+            print(f"  Genres: {chunk.get('genres', [])}")
+            print(f"  Chunk Index: {chunk.get('chunk_index', '')}")
+            print(f"  Introduction Chunk: {chunk.get('introduction', '')}")
     else:
         print("생성된 Chunk 데이터가 없습니다.")
 
+
 if __name__ == "__main__":
-    chunk_file_by_line()
-    print_all_chunk_data()
+    CSV_PATH = "../aladin_bestseller.csv"
+
+    try:
+        book_df = pd.read_csv(CSV_PATH, dtype=str)
+        print(f"파일 읽기 완료. 총 {len(book_df)} 행.")
+        print('='*50)
+        # chunking
+        all_chunks = chunk_file_by_line(book_df)
+        print(f"\n생성된 총 청크(행) 개수: {len(all_chunks)}")
+        print_all_chunk_data(all_chunks)
+        print('='*50)
+
+    except FileNotFoundError:
+        print(f"Error: '{CSV_PATH}'을 찾을 수 없음. 경로 확인 바람.")
+    except pd.errors.EmptyDataError:
+         print(f"Error: '{CSV_PATH}'이 비어있음.")
+    except Exception as e:
+        print(f"{CSV_PATH} 파일을 읽는 중 오류 발생: {e}")
