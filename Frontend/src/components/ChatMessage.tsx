f262e04c52f2f5c27e8d71a9284df72abfe7e34d
Ahn <2012726@naver.com>
2025-06-05 03:07:22 +0900

    하트 오류 해결

--- Frontend/src/components/ChatMessage.tsx
+++ Frontend/src/components/ChatMessage.tsx
@@ -1,31 +1,37 @@
 import "./ChatMessage.css"
 import BookRecommendationItem from "./BookRecommendationItem";
 
+interface Book {
+  title: string;
+  author: string;
+  summary: string;
+  recommendation: string;
+  isbn: string;
+  image: string;
+}
+
 interface Props {
   sender: "user" | "bot";
   text: string;
+  bookList?: Book[]; // 
 }
 
-export default function ChatMessage({ sender, text }: Props) {
+export default function ChatMessage({ sender, text, bookList }: Props) {
   const isBot = sender === "bot";
-  const avatarSrc = isBot ? "../../public/bot.jpg" : "../../public/user.jpg";
 
-  if (sender === "bot" && text.includes("1.")) {
-    // 추천 메시지인 경우 파싱하여 BookRecommendationItem으로 출력
-    const lines = text.split("\n").filter((line) => line.trim() !== "");
-    const header = lines[0];
-    const items = lines.slice(1);
-
+  const isRecommendation = sender === "bot" && bookList && bookList.length > 0;
+  
+  if (isRecommendation) {
     return (
         <div className={`chat-message ${sender}`}>
-          <div className="chat-avatar-container">
-            {/* <img src={avatarSrc} alt="avatar" className="chat-avatar" /> */}
-          </div>
           <div className={`chat-bubble ${sender}`}>
             <div className="chat-text">
-              <div style={{ marginBottom: "0.5rem" }}>{header}</div>
-              {items.map((line, idx) => (
-                <BookRecommendationItem key={idx} title={line} />
+              <div style={{ marginBottom: "0.5rem" }}>{text}</div>
+
+
+              {isRecommendation &&
+              bookList!.map((book, idx) => (
+              <BookRecommendationItem key={idx} book={book} />
             ))}
           </div>
         </div>
@@ -36,9 +42,6 @@ export default function ChatMessage({ sender, text }: Props) {
   // 일반 메시지 출력
   return (
       <div className={`chat-message ${sender}`}>
-        <div className="chat-avatar-container">
-          {/* <img src={avatarSrc} alt="avatar" className="chat-avatar" /> */}
-        </div>
         <div className={`chat-bubble ${sender}`}>
         <div className="chat-text">{text}</div>
       </div>
